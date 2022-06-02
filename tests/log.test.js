8de020d75ec3502959fc35de30dbfa85e1a2781e
bilingual-vovs <vovavovsart@gmail.com>
2022-06-02 18:56:43 +0300

    1.2.2beta realise

--- tests/log.test.js
+++
@@ -1,6 +0,0 @@
-const log = require('../functions/log')
-
-log("Hello test", "red", "bgblue", "./logs/test.log")
-log("Hello test", "red", "./logs/test.log", "-f")
-log("Hello test", "UnderScore", "./logs/test.log", 112)
-log("Hello test", "blue", "./logs/test.log", {aaa: "red"}, "./logs/test1.log")
--- tests/stringing.test.js
+++
@@ -1,9 +0,0 @@
-const console = require('../index')
-
-console.object({
-    a: "a",
-    b: 2
-})
-
-console.number(2)
-console.arr([11, "33"])
